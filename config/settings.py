5b6115bbc6f479a8967e7e77213485a41feda692
June Roh <texasroh@gmail.com>
2022-06-16 01:57:26 -0400

    users app init

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ DJANGO_APPS = [
 THIRDPARTY_APPS = []
 PROJECT_APPS = [
     "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRDPARTY_APPS + PROJECT_APPS
@@ -132,3 +133,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
---
+++ users/models.py
@@ -0,0 +1,21 @@
+from core import models as core_models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser, core_models.TimeStampModel):
+
+    LOGIN_KAKAO = "kakao"
+    LOGIN_GOOGLE = "google"
+    LOGIN_EMAIL = "email"
+    LOGIN_CHOICES = (
+        (LOGIN_KAKAO, "Kakao"),
+        (LOGIN_GOOGLE, "Google"),
+        (LOGIN_EMAIL, "Email"),
+    )
+
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=120, default="", blank=True)
+    login_method = models.CharField(
+        choices=LOGIN_CHOICES, max_length=10, default=LOGIN_EMAIL
+    )
